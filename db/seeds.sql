670c58dc2d54f78f481c1550070ea11425510997
Tizoc <jcondonpro@gmail.com>
2021-01-28 15:21:24 -0800

    cats to burgers

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('The Charlie Horse');
+INSERT INTO burgers (name) VALUES ('Guacamole Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Crunchy Munch', true);
+INSERT INTO burgers (name, devoured) VALUES ('Baconator', true);
+INSERT INTO burgers (name, devoured) VALUES ('Delish Dish', true);
